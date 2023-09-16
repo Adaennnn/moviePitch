f5fbee806dc7a83ece4d17216851b7bf9a8a7223
adaen <adaen@adaen.myguest.virtualbox.org>
2023-09-16 18:45:02 +0300

    Updated configuration for secure deployment

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 export const process = {
     env: {
-        OPENAI_API_KEY: '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'
+        OPENAI_API_KEY: window.OPENAI_API_KEY || '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'
     }
 }
